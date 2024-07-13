1cf8f0971a96212a8b1170b1119635d14f4f1c7b
garold664 <garold664@gmail.com>
2024-07-13 20:45:15 +0300

    adds react-router and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
 
 import MotionTranstion from './components/MotionTransiton';
+import { NavLink } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,7 +17,24 @@ function App() {
           gap: '24px',
         }}
       >
-        <MotionTranstion />
+        <nav
+          style={{
+            position: 'fixed',
+            inset: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            gap: '2rem',
+            alignItems: 'center',
+            height: '5vh',
+            background: 'rgba(0,0,0,0.5)',
+          }}
+        >
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/motion-transition">Motion Transition</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/motion-transition" element={<MotionTranstion />} />
+        </Routes>
       </main>
     </>
   );
